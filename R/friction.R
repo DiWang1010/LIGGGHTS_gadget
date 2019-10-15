46079d84fa020e6f88989732b1c7eed976d64df7
DiWang1010 <wangdi819825@gmail.com>
2019-10-15 10:36:09 +0200

    add xlim,ylim

--- R/friction.R
+++ R/friction.R
@@ -1,6 +1,6 @@
 library(ggplot2)
 friction=read.table("friction.data")
 p<-ggplot(data=friction,aes(x=V1, y=V2))+geom_line()+geom_path(size=1)+geom_point(size=1,color='darkblue')
-p+labs(title = "Loading curve",x = "Steps",y = "Friction ratio")
+p+labs(title = "Loading curve",x = "Steps",y = "Friction ratio")+xlimt(0,1e9)+ylimt(0.3,0.6)
 ggsave("Loading.png",path = "/home/wd/",width = 20, height = 20, units = "cm")
 
